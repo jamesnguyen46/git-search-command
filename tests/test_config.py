2b214f4333693c1e6c19af6f277aaac4b327a206
jamesnguyen46 <thachnguyen1989@gmail.com>
2022-07-07 09:19:41 +0700

    Temp

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,16 @@
+from gsc import config
+
 # Test BaseConfig
+MOCK_ENV_FILE_NAME = "test"
+mock_data = {"key1": "value1", "key2": "value2"}
+
+
+def test_get_key(mocker):
+    mocker.patch("dotenv.dotenv_values", return_value=mock_data)
+    baseconfig = config.BaseConfig(MOCK_ENV_FILE_NAME)
+    assert baseconfig.get_key("key1") == mock_data["key1"]
+    assert baseconfig.get_key("key2") == mock_data["key2"]
+
 
 # Test AppConfig
 
